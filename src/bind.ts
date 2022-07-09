80e3bde856e1db0c2aa691b0d4f744912c4f35bb
Travis Arndt <tmarndt1991@gmail.com>
2022-07-09 16:37:55 -0400

    Added ReactiveState

--- src/bind.ts
+++ src/bind.ts
@@ -4,31 +4,45 @@ type Pick<T, K extends keyof T> = {
     [P in K]: string;
 };
 
-type Resolver<T> = (state: Readonly<T>) => (Pick<T, keyof T>);
-
-export function bind<TState extends ReactState, K extends keyof TState>(state: TState, resolver: Pick<TState, K>): ClassDecorator {
+export function bind<TState extends ReactiveState, K extends keyof TState>(state: TState, map: Pick<TState, K>): ClassDecorator {
     return function<T>(constructor: Function) {
-        if (state instanceof ReactState && React.Component.prototype.isPrototypeOf(constructor.prototype)) {
-            state["_components"].push(constructor.prototype);
+        if (state instanceof ReactiveState && React.Component.prototype.isPrototypeOf(constructor.prototype)) {
+            state["_resolvers"].push([ constructor.prototype, map ]);
         }
     }
 }
 
-export class ReactState {
-    private _components: React.Component[] = [];
-    public test: string;
-    public whatever: string;
+function define(state: ReactiveState, key: string) {
+    let value: any;
 
+    const getter = function() {
+        return value;
+    }
+
+    const setter = function(newVal: any) {
+        value = newVal;
+
+        state["_resolvers"]?.forEach(resolver => {
+            if (resolver[1].hasOwnProperty(key)) {
+                resolver[0].setState({
+                    key, value
+                });
+            }
+        });
+    }
+
+    Object.defineProperty(state, key, {
+        get: getter,
+        set: setter
+    });
 }
 
-interface IState {
-    name: string;
-}
+export class ReactiveState {
+    private _resolvers: [ component: React.Component, map: Object ][] = [];
 
-@bind(new ReactState(), {
-    whatever: "",
-    test: ""
-})
-class Component extends React.Component<{}, IState> {
-    
+    constructor() {
+        for (let key in this) {
+            define(this, key);
+        }
+    }
 }
